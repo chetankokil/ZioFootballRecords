8afbb25e814ce3c53311642f521dbd3a022009d0
chetankokil <chetankokil@gmail.com>
2021-06-19 20:46:57 -0500

    added .scalafmt.conf

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,7 @@
 import io.circe.generic.auto._
 import sttp.client3.circe.asJson
 import zio._
-import zio.json.{DeriveJsonCodec, DeriveJsonDecoder}
+import zio.json.{ DeriveJsonCodec, DeriveJsonDecoder }
 
 trait FootBallService {
   def findAllByYear(year: Int): Task[FootBallData]
@@ -11,11 +11,11 @@ object FootBallService {
   val live: ZLayer[Any, Nothing, Has[FootBallService]] = ZLayer.succeed(new FootBallService {
     override def findAllByYear(year: Int) = Task {
       import sttp.client3._
-      val queryMap = Map("year" -> year, "page" -> 1)
-      val uri = uri"https://jsonmock.hackerrank.com/api/football_matches?$queryMap"
+      val queryMap         = Map("year" -> year, "page" -> 1)
+      val uri              = uri"https://jsonmock.hackerrank.com/api/football_matches?$queryMap"
       implicit val backend = HttpURLConnectionBackend()
-      val request = basicRequest.get(uri).response(asJson[FootBallData].getRight)
-      val resposnse = request.send(backend)
+      val request          = basicRequest.get(uri).response(asJson[FootBallData].getRight)
+      val resposnse        = request.send(backend)
       resposnse.body
     }
   })
@@ -25,16 +25,16 @@ object FootBallService {
 }
 
 object FootBallDataJson {
-  implicit val dataJson = DeriveJsonCodec.gen[Data]
-  implicit val footBallDataJson = DeriveJsonCodec.gen[FootBallData]
+  implicit val dataJson            = DeriveJsonCodec.gen[Data]
+  implicit val footBallDataJson    = DeriveJsonCodec.gen[FootBallData]
   implicit val footBallJsonDecoder = DeriveJsonDecoder.gen[FootBallData]
 }
-
 
 object Main extends App {
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
-    FootBallService.findAllByYear(2011)
+    FootBallService
+      .findAllByYear(2011)
       .provideLayer(FootBallService.live)
       .map(fl => println(fl))
       .exitCode
